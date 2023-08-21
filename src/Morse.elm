bfd6d25c912d1c960f461c2bf24e0d5fb19222cf
Garrett van Wageningen <ghvanw283@gmail.com>
2023-08-20 23:04:43 -0500

    update morseinfo to support fromText func

--- src/Morse.elm
+++ src/Morse.elm
@@ -6,52 +6,61 @@ import Alphabet exposing (..)
 type alias MorseInfo =
     { wordSeparator : String
     , letterSeparator : String
-    , morseToAlpha : String -> Char
+
+    -- , morseToAlpha : String -> Char
     }
 
 
-toText : MorseInfo -> (String -> String)
-toText morseInfo =
+toText : ( MorseInfo, String -> Char ) -> (String -> String)
+toText ( morseInfo, morseToAlpha ) =
     String.split morseInfo.wordSeparator
-        >> List.map (String.split morseInfo.letterSeparator >> List.map morseInfo.morseToAlpha >> String.fromList)
+        >> List.map (String.split morseInfo.letterSeparator >> List.map morseToAlpha >> String.fromList)
         >> String.join " "
 
 
 codeToText : String -> String
 codeToText =
     toText
-        { wordSeparator = "\t"
-        , letterSeparator = " "
-        , morseToAlpha = Alphabet.morseToAlpha >> Maybe.withDefault '_'
-        }
+        ( { wordSeparator = "\t"
+          , letterSeparator = " "
+          }
+        , Alphabet.morseToAlpha >> Maybe.withDefault '_'
+        )
 
 
 whitespaceToText : String -> String
 whitespaceToText =
     toText
-        { wordSeparator = "\t\t\t\t\t\t\t"
-        , letterSeparator = "\t\t\t"
-        , morseToAlpha = Alphabet.whitespaceToAlpha >> Maybe.withDefault '_'
-        }
+        ( { wordSeparator = "\t\t\t\t\t\t\t"
+          , letterSeparator = "\t\t\t"
+          }
+        , Alphabet.whitespaceToAlpha >> Maybe.withDefault '_'
+        )
 
 
-fromText : (Char -> String) -> (String -> String)
-fromText convert =
+fromText : ( MorseInfo, Char -> String ) -> (String -> String)
+fromText ( morseInfo, convert ) =
     let
         wordToMorse : String -> String
         wordToMorse =
-            String.toList >> List.map convert >> String.join " "
+            String.toList >> List.map convert >> String.join morseInfo.letterSeparator
     in
     String.words
         >> List.map wordToMorse
-        >> String.join "\t"
+        >> String.join morseInfo.wordSeparator
 
 
 codeFromText : String -> String
 codeFromText =
-    fromText (Alphabet.alphaToMorse >> Maybe.withDefault "_")
+    fromText
+        ( { wordSeparator = "\t", letterSeparator = " " }
+        , Alphabet.alphaToMorse >> Maybe.withDefault "_"
+        )
 
 
 whitespaceFromText : String -> String
 whitespaceFromText =
-    fromText (Alphabet.alphaToMorse >> Maybe.withDefault "_")
+    fromText
+        ( { wordSeparator = "\t\t\t\t\t\t\t", letterSeparator = "\t\t\t" }
+        , Alphabet.alphaToWhitespace >> Maybe.withDefault "_"
+        )
--- tests/GivenAlphaText.elm
+++ tests/GivenAlphaText.elm
@@ -20,4 +20,39 @@ alphaToMorse =
                         Morse.codeFromText text
                 in
                 result |> Expect.equal ".... . .-.. .-.. ---\t.-- --- .-. .-.. -.."
+        , test "when converting text to Whitespace morse code" <|
+            \_ ->
+                let
+                    text =
+                        "hello world"
+
+                    hello =
+                        " \t \t \t "
+                            ++ "\t\t\t"
+                            ++ " "
+                            ++ "\t\t\t"
+                            ++ " \t   \t \t "
+                            ++ "\t\t\t"
+                            ++ " \t   \t \t "
+                            ++ "\t\t\t"
+                            ++ "   \t   \t   "
+
+                    world =
+                        " \t   \t   "
+                            ++ "\t\t\t"
+                            ++ "   \t   \t   "
+                            ++ "\t\t\t"
+                            ++ " \t   \t "
+                            ++ "\t\t\t"
+                            ++ " \t   \t \t "
+                            ++ "\t\t\t"
+                            ++ "   \t \t "
+
+                    expectedResult =
+                        hello ++ "\t\t\t\t\t\t\t" ++ world
+
+                    result =
+                        Morse.whitespaceFromText text
+                in
+                result |> Expect.equal expectedResult
         ]
